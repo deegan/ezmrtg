836a9f2b2aa78a9710cea05d4328551a390e4371
Harry Houdini <hakan@monkii.net>
2021-04-02 21:13:25 +0200

    ..

--- scripts/make-left.sh
+++ scripts/make-left.sh
@@ -5,5 +5,5 @@ source config
 rm $MRTG_ROOT/left.html
 
 for dir in `ls $MRTG_ROOT/data`; do
-    echo "<a href="$MRTG_ROOT/data/$dir/index.html" target="main">$dir</a><br>" >> $MRTG_ROOT/left.html
+    echo "<a href="/data/$dir/index.html" target="main">$dir</a><br>" >> $MRTG_ROOT/left.html
 done
